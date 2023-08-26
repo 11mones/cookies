df3ee7e6272fcb2a076205a840c2e63f5a6c6d2d
11mones <mones_leader@hotmail.com>
2023-08-26 13:52:11 +0000

    for deployment 8

--- project/settings.py
+++ project/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","0.0.0.0"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","0.0.0.0" , ".vercel/app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
